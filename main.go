0c5141b321844533818a8f9ebc25afa1d11257af
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-21 18:37:57 -0500

    Nested structs

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	// alex := person{firstName: "Alex", lastName: "Anderson"}
-	// fmt.Println(alex)
-
-	var alex person
-
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
-	// "%+v" will print out all the field names and values from alex.
+	jim := person{
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo{
+			email: "jim.party@gmail.com",
+			zipCode: 94000,
+		}, // All lines need commas
+	}
+	fmt.Printf("%+v", jim)
 }
