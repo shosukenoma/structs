fd59106dd25d36eb1fd3ca249c863e63ab458462
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-21 22:54:24 -0500

    Pointers

--- main.go
+++ main.go
@@ -23,9 +23,35 @@ func main() {
 		}, // All lines need commas
 	}
 
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+
+	// pointer shortcut
+	// if receiver function has *person, jim is automatically received as &jim.
+	// jim.updateName("Jimmy")
+
 	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+	// p is a copy of "jim" that points to a different memory location
 }
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
 }
+
+// &jim - memory address (pointer)
+// provides the memory address where jim is pointing at.
+
+// *pointerToPerson - value
+// provides the value to which this memory adress is pointing at.
+
+// *person - type: pointer to a person
+// different from above two! Declares that the type is a "pointer to a person"
+
+// Note: This only applies to structs.
+// No need to worry for slices.
+// This is because slices are reference data structures that point to an underlying array.
+// The slices are copied, but the underlying array is the same, and both copies point to the same array.
